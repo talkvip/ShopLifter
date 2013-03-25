b656436e7b49525271fea2620e4dc64962985b6c
Andrew Won <andyjwon@gmail.com>
2013-03-25 10:16:47 -0700

    Expanded item resource test coverage

--- webservice/src/test/java/edu/lmu/cs/eccms/ws/resource/ItemResourceTest.java
+++ webservice/src/test/java/edu/lmu/cs/eccms/ws/resource/ItemResourceTest.java
@@ -3,6 +3,8 @@ package edu.lmu.cs.eccms.ws.resource;
 import java.util.List;
 
 import junit.framework.Assert;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
@@ -18,73 +20,71 @@ import edu.lmu.cs.eccms.ws.util.DomainObjectUtils;
 public class ItemResourceTest extends ResourceTest {
 
     private Item testItem = new Item(1000000L, true, "MacBook Air", 135, "Computer thingy.", 1199.98, null, null);
-    private Item itemToCreate1 = DomainObjectUtils.createItemObject(true, "Andy", 123, "This is me", 0.99);
-    private Item itemToCreate2 = DomainObjectUtils.createItemObject(null, "SamV", 8675309, "What did I break?", 0.53);
+    private Item itemToCreate = DomainObjectUtils.createItemObject(false, "Big Bad Box", 134, "8gb memory and more!",
+            399.99);
+    private Item itemToUpdate = DomainObjectUtils.createItemObject(false, "Big Bad Box", 134, "8gb memory and more!",
+            399.99);
 
     @Test
     public void testGetItemByNonexistentId() {
         ClientResponse clientResponse = wr.path("items/17").get(ClientResponse.class);
         Assert.assertEquals(204, clientResponse.getStatus());
     }
-    /*
+
     @Test
     public void testGetItemById() {
         Item item = wr.path("items/1000000").get(ClientResponse.class).getEntity(Item.class);
-        Assert.assertTrue(testItem.equals(item));
+        assertThat(item, is(testItem));
     }
 
     @Test
     public void testGetItemsQueryByName() {
-        List<Item> items = wr.path("items").queryParam("q", "leonard")
-                .get(new GenericType<List<Item>>() { });
+        List<Item> items = wr.path("items").queryParam("q", "Air").get(new GenericType<List<Item>>() {
+        });
 
         Assert.assertEquals(1, items.size());
     }
 
     @Test
-    public void testGetItemsQueryByTitle() {
-        List<Item> items = wr.path("items").queryParam("q", "worldwide")
-                .get(new GenericType<List<Item>>() { });
+    public void testGetItemsQueryByDescription() {
+        List<Item> items = wr.path("items").queryParam("q", "thingy").get(new GenericType<List<Item>>() {
+        });
 
         Assert.assertEquals(1, items.size());
     }
 
-    @Test(expected=com.sun.jersey.api.client.UniformInterfaceException.class)
+    @Test(expected = com.sun.jersey.api.client.UniformInterfaceException.class)
     public void testGetItemsByNullQuery() {
-        List<Item> items = wr.path("items")
-                .get(new GenericType<List<Item>>() { });
+        List<Item> items = wr.path("items").get(new GenericType<List<Item>>() {
+        });
 
         Assert.assertEquals(1, items.size());
     }
 
     @Test
-    public void testGetItemsByBooleanQuery() {
-        List<Item> items = wr.path("items").queryParam("presented", "false")
-                .get(new GenericType<List<Item>>() { });
+    public void testGetItemsQueryByNonExistingName() {
+        List<Item> items = wr.path("items").queryParam("q", "Dell").get(new GenericType<List<Item>>() {
+        });
 
-        Assert.assertEquals(2, items.size());
+        Assert.assertEquals(0, items.size());
     }
 
     @Test
-    public void testGetItemsByAwardedQuery() {
-        List<Item> items = wr.path("items").queryParam("awarded", "AWARDED")
-                .get(new GenericType<List<Item>>() { });
+    public void testGetItemsByActiveQuery() {
+        List<Item> items = wr.path("items").queryParam("active", "true").get(new GenericType<List<Item>>() {
+        });
 
         Assert.assertEquals(1, items.size());
     }
 
     @Test
     public void testCreateItem() {
-        Item itemToCreate = DomainObjectUtils.createItemObject(1024, "Tyler Nichols", "Marc Papkyriakou");
-
         ClientResponse response = wr.path("items").post(ClientResponse.class, itemToCreate);
         Assert.assertEquals(201, response.getStatus());
     }
 
     @Test
     public void testCreateItemWithSpecifiedIdProduces400() {
-        Item itemToCreate = DomainObjectUtils.createItemObject(1024, "Tyler Nichols", "Marc Papkyriakou lol");
-
         itemToCreate.setId(500L);
 
         ClientResponse response = wr.path("items").post(ClientResponse.class, itemToCreate);
@@ -93,20 +93,18 @@ public class ItemResourceTest extends ResourceTest {
 
     @Test
     public void testUpdateItemProduces204() {
-        Item itemToUpdate = DomainObjectUtils.createItemObject(1000, "Andy Won", "Won Item");
-
         itemToUpdate.setId(1000000L);
 
         ClientResponse response = wr.path("items/1000000").put(ClientResponse.class, itemToUpdate);
         Assert.assertEquals(204, response.getStatus());
+
+        Item updatedItem = wr.path("items/1000000").get(ClientResponse.class).getEntity(Item.class);
+        assertThat(updatedItem, is(itemToUpdate));
     }
 
     @Test
     public void testUpdateItemWithInconsistentIdProduces400() {
-        Item itemToUpdate = DomainObjectUtils.createItemObject(1000, "Andy Won", "Won Item");
-
         ClientResponse response = wr.path("items/1000").put(ClientResponse.class, itemToUpdate);
         Assert.assertEquals(400, response.getStatus());
     }
-*/
 }
