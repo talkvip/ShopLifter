c8f58214aa8e8b39fcc005301d81036c8027d0dd
Andrew Won <andyjwon@gmail.com>
2013-05-03 12:48:11 -0700

    Added inventory.js for inventory page client-side handling

---
+++ webclient/src/main/resources/edu/lmu/cs/eccms/js/inventory.js
@@ -0,0 +1,45 @@
+(function () {
+    var testData = [
+        {
+            name:"Water Bottle",
+            sku:23,
+            description:"Plastic",
+            price:3.4,
+            dimension:{},
+            itemHistory:{}
+        },{
+            name:"Paper Plates",
+            sku:42,
+            description:"Paper",
+            price:5.2,
+            dimension:{},
+            itemHistory:{}
+        }];
+
+        loadArrayToTable = function (arr) {
+                for (i = 0; i < arr.length; i++) {
+                    $("#inv-table > tbody:last").after(
+                        "<tr>\n" +
+                        "<td>" + arr[i].sku + "</td>\n" +
+                        "<td>" + arr[i].name + "</td>\n" +
+                        "<td>" + arr[i].price + "</td>\n" +
+                        "<td>" + "NA" + "</td>\n" +
+                        "<td>" + arr[i].description + "</td>\n" +
+                        "</tr>\n"
+                    );
+                }
+            },
+
+    loadJsonArray = function () {
+        $.getJSON(
+            "/relay?s=items?q=",
+            {},
+            function (array, textStatus, jqXHR) {
+                loadArrayToTable(array);
+            }
+        );
+    };
+
+    loadArrayToTable(testData);
+    loadJsonArray();
+})();
