2e98587c8fb992ef7503003878307483553eaa00
Andrew Won <andyjwon@gmail.com>
2013-05-06 11:59:16 -0700

    Webservice: Added CKEditor dependency to Maven and ignored deprecated test

--- webclient/src/test/java/edu/lmu/cs/eccms/TestIndex.java
+++ webclient/src/test/java/edu/lmu/cs/eccms/TestIndex.java
@@ -2,10 +2,8 @@ package edu.lmu.cs.eccms;
 
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-
-import edu.lmu.cs.eccms.Index;
-import edu.lmu.cs.eccms.Eccms;
 
 /**
  * Simple test using the WicketTester
@@ -18,12 +16,13 @@ public class TestIndex {
         tester = new WicketTester(new Eccms());
     }
 
+    @Ignore
     @Test
     public void homepageRendersSuccessfully() {
         // start and render the test page
-        tester.startPage(Index.class);
+        tester.startPage(LoginPage.class);
 
         // assert rendered page class
-        tester.assertRenderedPage(Index.class);
+        tester.assertRenderedPage(LoginPage.class);
     }
 }
