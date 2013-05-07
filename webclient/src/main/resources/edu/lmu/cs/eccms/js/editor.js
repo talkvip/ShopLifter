19352ed7d21681521477a107b345455f648a2865
Andrew Won <andyjwon@gmail.com>
2013-05-06 21:29:12 -0700

    Webclient: Added disabling of div edit/ ckedit toggle buttons that are inactive

--- webclient/src/main/resources/edu/lmu/cs/eccms/js/editor.js
+++ webclient/src/main/resources/edu/lmu/cs/eccms/js/editor.js
@@ -5,59 +5,63 @@
  */
 (function () {
     var CkEditor = {
-            turnOn: function () {
-                var ids = [],
-                    id;
-                $(".inactive-drawing-area").find("div").each(function(){ ids.push(this.id); });
+        turnOn : function () {
+            var ids = [], id;
+            $(".inactive-drawing-area").find("div").each(function () {
+                ids.push(this.id);
+            });
 
-                for (var i = 0; i < ids.length; i++) {
-                    // Make sure we only instantiate CKEditor for divs without instances
-                    if (CKEDITOR.instances[ids[i]]) {
-                        continue;
-                    }
-                    id = '#' + ids[i];
-
-                    if($(id).is(':empty')) {
-                        $(id).html('<p>Insert text here.</p>');
-                    }
-
-                    $(id).attr('contenteditable', 'true');
-
-                    CKEDITOR.inline(ids[i]);
+            for ( var i = 0; i < ids.length; i++) {
+                // Make sure we only instantiate CKEditor for divs without
+                // instances
+                if (CKEDITOR.instances[ids[i]]) {
+                    continue;
                 }
-            },
-            turnOff: function () {
-                var ids = [];
-                $(".inactive-drawing-area").find("div").each(function(){ ids.push(this.id); });
-                for (var i = 0; i < ids.length; i++) {
-                    if (CKEDITOR.instances[ids[i]]) {
-                        CKEDITOR.instances[ids[i]].destroy();
-                    }
-                }
-                $('.inactive-box')
-                    .attr('contenteditable', 'false');
-                for (i in CKEDITOR.instances) {
+                id = '#' + ids[i];
 
+                if ($(id).is(':empty')) {
+                    $(id).html('<p>Insert text here.</p>');
+                }
+
+                $(id).attr('contenteditable', 'true');
+
+                CKEDITOR.inline(ids[i]);
+            }
+        },
+        turnOff : function () {
+            var ids = [];
+            $(".inactive-drawing-area").find("div").each(function () {
+                ids.push(this.id);
+            });
+            for ( var i = 0; i < ids.length; i++) {
+                if (CKEDITOR.instances[ids[i]]) {
+                    CKEDITOR.instances[ids[i]].destroy();
                 }
             }
-        };
+            $('.inactive-box').attr('contenteditable', 'false');
+        }
+    };
 
-    $('#edit_sample_page').on('click', function() {
+    $('#edit_sample_page').on('click', function () {
         window.location.href = "editor";
     });
 
-    $('#draw_off').on('click', function() {
+    $('#draw_off').on('click', function () {
         Boxes.turnOff();
         CkEditor.turnOn();
+        $('#draw_off').attr('disabled', 'disabled');
+        $('#draw_on').removeAttr('disabled');
     });
 
-    $('#draw_on').on('click', function() {
+    $('#draw_on').on('click', function () {
         CkEditor.turnOff();
         Boxes.turnOn();
+        $('#draw_on').attr('disabled', 'disabled');
+        $('#draw_off').removeAttr('disabled');
     });
 
-    $('#save').on('click', function() {
-        for (var i in CKEDITOR.instances) {
+    $('#save').on('click', function () {
+        for ( var i in CKEDITOR.instances) {
             console.log(CKEDITOR.instances[i].getData());
             console.log(CKEDITOR.plugins.undo.UndoManager.save());
         }
