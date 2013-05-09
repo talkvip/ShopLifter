45fb7627f673ad6f10b557f6bbe4c639abddcdbb
Andrew Won <andyjwon@gmail.com>
2013-05-09 07:34:37 -0700

    Webclient: Saved wysiwyg data to JSON object

--- webclient/src/main/resources/edu/lmu/cs/eccms/js/editor.js
+++ webclient/src/main/resources/edu/lmu/cs/eccms/js/editor.js
@@ -4,43 +4,52 @@
  * @author Andrew Won
  */
 (function () {
-    var CkEditor = {
-        turnOn : function () {
-            var ids = [], id;
-            $(".inactive-drawing-area").find("div").each(function () {
-                ids.push(this.id);
-            });
-
-            for ( var i = 0; i < ids.length; i++) {
-                // Make sure we only instantiate CKEditor for divs without
-                // instances
-                if (CKEDITOR.instances[ids[i]]) {
-                    continue;
+    var getDivCss = function (id) {
+            var jObject = $('#' + id);
+            return {
+                    "top" : jObject.css('top'),
+                    "left" : jObject.css('left'),
+                    "width" : jObject.css('width'),
+                    "height" : jObject.css('height')
                 }
-                id = '#' + ids[i];
+            },
+        CkEditor = {
+            turnOn : function () {
+                var ids = [], id;
+                $(".inactive-drawing-area").find("div").each(function () {
+                    ids.push(this.id);
+                });
 
-                if ($(id).is(':empty')) {
-                    $(id).html('<p>Insert text here.</p>');
+                for ( var i = 0; i < ids.length; i++) {
+                    // Make sure we only instantiate CKEditor for divs without
+                    // instances
+                    if (CKEDITOR.instances[ids[i]]) {
+                        continue;
+                    }
+                    id = '#' + ids[i];
+
+                    if ($(id).is(':empty')) {
+                        $(id).html('<p>Insert text here.</p>');
+                    }
+
+                    $(id).attr('contenteditable', 'true');
+
+                    CKEDITOR.inline(ids[i]);
                 }
-
-                $(id).attr('contenteditable', 'true');
-
-                CKEDITOR.inline(ids[i]);
+            },
+            turnOff : function () {
+                var ids = [];
+                $(".inactive-drawing-area").find("div").each(function () {
+                    ids.push(this.id);
+                });
+                for ( var i = 0; i < ids.length; i++) {
+                    if (CKEDITOR.instances[ids[i]]) {
+                        CKEDITOR.instances[ids[i]].destroy();
+                    }
+                }
+                $('.inactive-box').attr('contenteditable', 'false');
             }
-        },
-        turnOff : function () {
-            var ids = [];
-            $(".inactive-drawing-area").find("div").each(function () {
-                ids.push(this.id);
-            });
-            for ( var i = 0; i < ids.length; i++) {
-                if (CKEDITOR.instances[ids[i]]) {
-                    CKEDITOR.instances[ids[i]].destroy();
-                }
-            }
-            $('.inactive-box').attr('contenteditable', 'false');
-        }
-    };
+        };
 
     $('#edit_sample_page').on('click', function () {
         window.location.href = "editor";
@@ -61,10 +70,18 @@
     });
 
     $('#save').on('click', function () {
-        for ( var i in CKEDITOR.instances) {
-            console.log(CKEDITOR.instances[i].getData());
-            console.log(CKEDITOR.plugins.undo.UndoManager.save());
+        var divs = {}, jObject, ids = [];
+        $(".inactive-drawing-area").find("div").each(function () {
+            ids.push(this.id);
+        });
+        for (var i = 0; i < ids.length; i++) {
+            divs[ids[i]] = getDivCss(ids[i]);
         }
-        alert('hi');
+        for ( var i in CKEDITOR.instances) {
+            var name = CKEDITOR.instances[i].name;
+            divs[name]["data"] = CKEDITOR.instances[i].getData();
+            //console.log(divs);
+        }
+        // TODO: divs now contains minimum saved data for wysiwyg
     });
 })();
